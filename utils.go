b52cd160f7742a26cbbe3163093c82680a093593
Buk Bukowski <38087302+bukowa@users.noreply.github.com>
2020-08-19 01:12:32 +0200

    Update utils.go

--- utils.go
+++ utils.go
@@ -3,35 +3,114 @@ package gowpcli
 import (
 	"context"
 	"fmt"
+	"regexp"
+
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
-func MakeArg(args []string, key string, value interface{}) []string {
-	if value == "" {
-		return append(args, fmt.Sprintf("--%s", key))
+// todo special treatment
+//"[--field=<field>]": "[--field=<field>]",
+//"[--<field>=<value>]": "[--<field>=<value>]",
+
+func formatKey(k string) string {
+	var r = []string{"[", "]", "..."}
+	for _, s := range r {
+		k = strings.Replace(k, s, "", -1)
 	}
-	return append(args, fmt.Sprintf("--%s=%s", key, value))
+	return k
+}
+
+func formatMap(m map[string]string) []string {
+	var s []string
+	for k, v := range m {
+		s = append(s, fmt.Sprintf("%s=%s", k, v))
+	}
+	return s
+}
+
+// formatNamed
+func formatNamed(k, v string) string {
+	var reg = regexp.MustCompile("(--.*?)\\=")
+	param := reg.Find([]byte(k))
+	return fmt.Sprintf("%s%s", param, v)
+}
+
+// isNamed checks if parameter is in form of `--name=`
+func isNamed(s string) (t bool) {
+	if strings.Contains(s, "--") {
+		t = true
+	}
+	return
+}
+
+func mult(s string) bool {
+	return strings.HasSuffix(s, "...") || strings.HasSuffix(s, "...]")
+}
+
+func isRequired(s string) bool {
+	return strings.HasPrefix(s, "<") || strings.HasPrefix(s, "--") || strings.HasPrefix(s, "[<")
+}
+
+func opt(s string) bool {
+	return strings.HasPrefix(s, "[")
+}
+
+
+func MakeArg(args []string, k string, v interface{}) []string {
+	Logger.Debugf("INPUT:%v, %v, %v", args, k, v)
+	k = formatKey(k)
+	switch t := v.(type) {
+	case map[string]string:
+		Logger.Debug("map[string]string")
+		if len(t) != 0 {
+			args = append(args, formatMap(t)...)
+		}
+	case []string:
+		Logger.Debug("[]string")
+		if len(t) != 0 {
+			args = append(args, t...)
+		}
+	case bool:
+		Logger.Debug("bool")
+		if t {
+			args = append(args, k)
+		}
+	case string:
+		Logger.Debug("string")
+		if t != "" {
+			if isNamed(k) {
+				Logger.Debug("is named")
+				args = append(args, formatNamed(k, t))
+			} else {
+				Logger.Debug("is not named")
+				args = append(args, t)
+			}
+		}
+	}
+	Logger.Debugf("RETURN:%v", args)
+	return args
 }
 
 func WaitForUrl(url string, status int, duration time.Duration) (ready bool) {
 
-	main_ctx, cancel_main := context.WithTimeout(context.Background(), duration)
-	defer cancel_main()
+	mainCtx, cancelMain := context.WithTimeout(context.Background(), duration)
+	defer cancelMain()
 
-	req_ctx, cancel_req := context.WithTimeout(context.Background(), duration)
-	defer cancel_req()
+	reqCtx, cancelReq := context.WithTimeout(context.Background(), duration)
+	defer cancelReq()
 
 	for {
 		select {
-		case <- main_ctx.Done():
-			log.Printf("main context timeout, context error: %v", main_ctx.Err())
+		case <- mainCtx.Done():
+			log.Printf("main context timeout, context error: %v", mainCtx.Err())
 			return
 		default:
 			client := &http.Client{}
 			req, _ := http.NewRequest(http.MethodGet, url, nil)
-			req = req.WithContext(req_ctx)
+			req = req.WithContext(reqCtx)
 
 			log.Printf("sending http request to %s", url)
 			res, err := client.Do(req)
@@ -50,4 +129,3 @@ func WaitForUrl(url string, status int, duration time.Duration) (ready bool) {
 		}
 	}
 }
-
